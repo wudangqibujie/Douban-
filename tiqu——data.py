d9968cdee5581e0ad42fbb74d8173a3fddc76dde
wudangqibujieshao <879303561@qq.com>
2018-03-29 17:00:00 +0800

    迁移

--- tiqu——data.py
+++ tiqu——data.py
@@ -1,7 +1,10 @@
 import redis
 import re
 rr = redis.Redis(host="localhost",port=6379)
-f = open("CAPOL.txt",encoding="utf-8")
-obj = f.read()
-print(re.findall(r'}{(.*?)}{',obj))
-
+f = open("CAPOL_urls.txt","w",encoding="utf-8")
+# obj = f.read()
+# print(re.findall(r'}{(.*?)}{',obj))
+# print(rr.spop("douban_urls").decode("utf-8"))
+while True:
+    f.write(rr.spop("douban_urls").decode("utf-8")+"\n")
+    print("11")
